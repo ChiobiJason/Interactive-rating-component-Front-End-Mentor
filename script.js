bb47a0eb8ff53a5b3fb0464752c1e71ac557ea31
ChiobiJason <chiobichisom@gmail.com>
2022-06-22 19:43:19 +0000

    Created buttons and add little styling

---
+++ script.js
@@ -0,0 +1,10 @@
+// const ratings = document.querySelectorAll(".rating");
+
+// const numberOfRatings = document.querySelectorAll(".rating").length;
+
+// for (let i = 0; i < numberOfRatings; i++) {
+//    ratings[i].addEventListener("click",  function() {
+//     this.style.background = "rgb(250 118 19)";
+// });
+    
+// }
